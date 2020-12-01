b4d9a1731939338d2781f8729f900be408ddcc90
Mario Gallo <marioffgallo@hotmail.com>
2020-12-01 12:00:46 -0300

    Adicionando endpoint para os posts dos usuários

--- src/main/java/com/marioffgallo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marioffgallo/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.marioffgallo.workshopmongo.domain.Post;
 import com.marioffgallo.workshopmongo.domain.User;
 import com.marioffgallo.workshopmongo.dto.UserDTO;
 import com.marioffgallo.workshopmongo.services.UserService;
@@ -60,5 +61,11 @@ public class UserResource {
 		obj.setId(id);
 		service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value="/{id}/posts")
+	public ResponseEntity<List<Post>> findPost(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
