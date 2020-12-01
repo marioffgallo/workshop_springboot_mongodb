7a8befe107a61d8cfa305aca5d8395f355644006
Mario Gallo <marioffgallo@hotmail.com>
2020-12-01 10:33:32 -0300

    UserExcepetions implementado

--- src/main/java/com/marioffgallo/workshopmongo/resources/UserResource.java
+++ src/main/java/com/marioffgallo/workshopmongo/resources/UserResource.java
@@ -1,14 +1,18 @@
 package com.marioffgallo.workshopmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.marioffgallo.workshopmongo.domain.User;
+import com.marioffgallo.workshopmongo.dto.UserDTO;
 import com.marioffgallo.workshopmongo.services.UserService;
 
 @RestController
@@ -19,8 +23,16 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
+	
+//	@GetMapping(value="/{id}")
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 }
